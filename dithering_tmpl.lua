849a92c2318a9ed560e1cf8ded59de81fdc1af2e
nssuperx <40332687+nssuperx@users.noreply.github.com>
2025-08-16 16:53:02 +0900

    少し修正

--- dithering_tmpl.lua
+++ dithering_tmpl.lua
@@ -1,4 +1,4 @@
-@dithering
+@通常
 --label:加工
 --track@fade_percentage:透明度,0,100,0,0.1
 --track@fineness:細かさ,0,100,100,1
@@ -8,11 +8,11 @@
 
 obj.pixelshader("dithering", "object", {"object"}, {fade_percentage, fineness}, "copy")
 
-@dithering_z
+@Z座標
 --label:加工
 --check@is_offscrean:オフスクリーン描画,1
 --track@start_z:開始Z座標,-1024,100000,-512,1
---track@end_z:終了Z座標,-1024,100000,-1024,1
+--track@end_z:終了Z座標,-1024,100000,-768,1
 --track@fineness:細かさ,0,100,100,1
 --[[pixelshader@dithering_z:
 {{.DitheringZ}}
